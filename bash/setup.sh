55e44d9d314da4e4c68b7bbc4806339979a6414a
HaochenPan <phchcc@gmail.com>
2019-06-05 07:58:34 -0400

    Update setup.sh

--- bash/setup.sh
+++ bash/setup.sh
@@ -34,14 +34,15 @@ install_cass() {
             ;;
         abd | abdOpt)
             git clone https://github.com/ZezhiWang/cassandra.git
+            cd cassandra
             git checkout $1
             ;;
         0d4* | treasErasure | ErasureMemory)  # 0d464cd25ffbb5734f96c3082f9cc35011de3667
             git clone https://github.com/yingjianwu199868/cassandra.git
+            cd cassandra
             git checkout $1
             ;;
     esac
-    cd cassandra
     ant build
     git status
     cd
